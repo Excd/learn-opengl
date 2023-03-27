d976f3da7b1e48fd842d3c18e7e04da0ccfc13d9
Excd <greggseamon@gmail.com>
2023-03-27 19:29:54 -0400

    Render simple GLFW window
    Create GLFW window and initialize glad.
    Set window resize callback.
    Create basic render loop.

--- projects/getting-started/src/main.cpp
+++ projects/getting-started/src/main.cpp
@@ -1,13 +1,57 @@
 ﻿#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+
+const unsigned int WINDOW_WIDTH = 800;
+const unsigned int WINDOW_HEIGHT = 600;
 
 int main(int argc, char *argv[]) {
-	glfwInit();	// Initialize GLFW.
-
-	// Configure GLFW version and profile.
+	// Initialize GLFW and configure OpenGL version and profile.
+	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	// Allow OpenGL forward compatibility on macOS.
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+	// Create GLFW window and set to current context.
+	GLFWwindow *window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "LearnOpenGL", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window." << std::endl;
+		glfwTerminate();
+
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Set GLFW window resize callback.
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// Initialize glad.
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD." << std::endl;
+
+		return -1;
+	}
+
+	// Main render loop.
+	while (!glfwWindowShouldClose(window)) {
+		glfwSwapBuffers(window);	// Swap window frame data buffers.
+		glfwPollEvents();			// Poll for IO events.
+	}
+
+	glfwTerminate(); // Terminate GLFW and free resources.
+
 	return 0;
+}
+
+// Callback function for GLFW window resize.
+// Ensures viewport matches window dimensions.
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+	glViewport(0, 0, width, height);
 }
