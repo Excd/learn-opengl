2de2148fef943bea1fd17840724747e9bb7d2797
Excd <greggseamon@gmail.com>
2023-04-17 21:45:10 -0400

    Add comments to hello-triangle project
    Add helpful comments pertaining to unbinding EBOs.

--- projects/getting-started/hello-triangle/src/main.cpp
+++ projects/getting-started/hello-triangle/src/main.cpp
@@ -133,6 +133,9 @@ int main(int argc, char *argv[]) {
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
 	glEnableVertexAttribArray(0);
 
+	// Do not unbind the EBO while a VAO is active as the bound EBO is stored within the VAO.
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	
 	// Unbind VBO, and VAO.
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
